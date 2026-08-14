fe1714bf8fd399d479b08b4f1a3277939fa0b9ea
Archita Singh <architas658@gmail.com>
2026-08-15 02:08:38 +0530

    Time: 8 ms (8.99%), Space: 8.6 MB (65.25%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int temp=x;
+        long long sum=0;
+        while(x>0){
+            int rem=x%10;
+            sum=sum*10+rem;
+            x=x/10;
+        }
+        if(sum==temp){
+            return true;
+        }
+        return false;
+    }
+};
