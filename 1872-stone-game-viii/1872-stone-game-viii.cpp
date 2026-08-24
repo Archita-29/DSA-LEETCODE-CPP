9280233acbbb4c7e60fc105047bef84531f70c84
Archita Singh <architas658@gmail.com>
2026-08-25 02:35:13 +0530

    Time: 111 ms (44.35%), Space: 87.2 MB (84.35%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n=stones.size();
+        for(int i=1;i<n;i++){
+            stones[i]+=stones[i-1];
+        }
+        int maxDifference=stones[n-1];
+        for(int i=n-2;i>=1;i--){
+            maxDifference=max(maxDifference,stones[i]-maxDifference);
+        }
+        return maxDifference;
+    }
+};
