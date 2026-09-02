ef2d920c793343d4660758a32fec18c985b01569
Archita Singh <architas658@gmail.com>
2026-09-03 00:12:09 +0530

    Time: 0 ms (100%), Space: 30.3 MB (67.8%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
+        int oddVal=-1;
+        for(int num:nums1){
+            if(num%2!=0){
+                oddVal=num;
+                break;
+            }
+        }
+        if(oddVal==-1){
+            return true;
+        }
         return true;
     }
 };
