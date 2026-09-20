f46c998ff5197cb0336b302a9d09cc9ed1129242
Archita Singh <architas658@gmail.com>
2026-09-20 19:06:42 +0530

    Time: 0 ms (100%), Space: 10.9 MB (74.33%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -13,24 +13,19 @@ class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int>result;
-        if(!root) return result;
-        stack<TreeNode*>st;
-        TreeNode* curr=root;
-        TreeNode* lastVisited=nullptr;
-        while(curr!=nullptr || !st.empty()){
-            if(curr!=nullptr){
-                st.push(curr);
-                curr=curr->left;
-            }else{
-                TreeNode* peekNode=st.top();
-                if(peekNode->right!=nullptr && lastVisited!=peekNode->right){
-                    curr=peekNode->right;
-                }else{
-                    result.push_back(peekNode->val);
-                    lastVisited=peekNode;
-                    st.pop();
-                }
-            }
+        if(root==NULL) return result;
+        stack<TreeNode*>st1,st2;
+        st1.push(root);
+        while(!st1.empty()){
+            root=st1.top();
+            st1.pop();
+            st2.push(root);
+            if(root->left!=NULL) st1.push(root->left);
+            if(root->right!=NULL) st1.push(root->right);
+        }
+        while(!st2.empty()){
+            result.push_back(st2.top()->val);
+            st2.pop();
         }
         return result;
     }
