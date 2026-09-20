0603e9836a76164d8f839bbe12c2436a6f57e681
Archita Singh <architas658@gmail.com>
2026-09-20 18:37:47 +0530

    Time: 0 ms (100%), Space: 9.9 MB (15.78%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        int rank[26];
-        for(int c=0;c<26;++c){
-            rank[c]=26-c;
-        }
         int total=0;
-        for(int i=0;i<(int)s.size();++i){
-            total+=rank[s[i]-'a']*(i+1);
+        for(int i=0;i<(int)s.size();i++){
+            total+=('z'-s[i]+1)*(i+1);
         }
         return total;
     }
